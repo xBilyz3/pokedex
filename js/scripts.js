6d94a71a6a707db18d072391bb59dcbc16bd34fa
Konrad Bil <52658352+xBilyz3@users.noreply.github.com>
2019-07-31 23:35:08 +0530

    update scripts.js
    --“forEach” function has been added that loops over each Pokémon in the array
    
    -- “forEach” function has been added that loops over each Pokémon type in the types:array

--- js/scripts.js
+++ js/scripts.js
@@ -19,14 +19,14 @@ var repository = [
   },
 ];
 
-for (var i = 0; i < repository.length; i++) {
-  document.write('<h1>' + repository[i].name + '</h1>');
-  document.write(repository[i].height);
-  for (var t = 0; t < repository[i].types.length; t++) {
-    var type = repository[i].types[t]
-    document.write('<div ' + 'class="' + type + '" > ' + type + ' </div>')
-  }
-  if (repository[i].height > 11) {
+repository.forEach(function(pokemonDetail) {
+  document.write('<h1>' + pokemonDetail.name + '</h1>');
+  document.write('<p>' + pokemonDetail.height) + '</p>';
+  if (pokemonDetail.height > 11) {
     document.write(' - Wow, that\’s big!');
   }
-}
+  var type = pokemonDetail.types;
+  type.forEach(function(pokemonType) {
+    document.write('<div ' + 'class="' + pokemonType + '" > ' + pokemonType + ' </div>');
+  });
+});
