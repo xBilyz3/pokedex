22d36ab457cded57c6cb855ef2d25851aeddf9c7
Konrad Bil <52658352+xBilyz3@users.noreply.github.com>
2019-08-04 00:45:37 +0530

    update scripts.js
    repository array wraped into IIFE to avoid accidentally accessing the global state.

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,68 @@
-var repository = [
-  // first pokemon
-  {
-    name: 'Bulbasaur',
-    height: 7,
-    types: ['grass', 'poisen']
-  },
-  // second pokemon
-  {
-    name: 'Charmander',
-    height: 12,
-    types: ['fire']
-  },
-  // third pokemon
-  {
-    name: 'Squirtle',
-    height: 4,
-    types: ['water']
-  },
-];
+var pokemonRepository = (function() {
+  var repository = [
+    // first pokemon
+    {
+      name: 'Bulbasaur',
+      height: 0.7,
+      types: ['grass', 'poisen']
+    },
+    // second pokemon
+    {
+      name: 'Charmander',
+      height: 0.6,
+      types: ['fire']
+    },
+    // third pokemon
+    {
+      name: 'Squirtle',
+      height: 0.5,
+      types: ['water']
+    },
+  ];
+
+  // add new pokemon  when modified with the correct type of data
+  // and Object.keys()of the parameter are equal to the specific keys
+  function add(pokemon) {
+    if (typeof pokemon === 'object' && (Object.keys(pokemon) === 'name' || 'height' || 'types')) {
+      repository.push(pokemon);
+    }
+  }
+
+  // pokemon types change font color
+  function getTextColor(pokemonType) {
+    return ' <div ' + 'class="' + pokemonType + '" >' + pokemonType + '</div>';
+  }
+
+  // return the whole repository
+  function getAll() {
+    return repository;
+  }
+
+
+  // exposed public functions
+  return {
+    add: add,
+    getAll: getAll,
+    getTextColor: getTextColor
+  };
+})();
+
+pokemonRepository.add();
+pokemonRepository.getAll();
+pokemonRepository.getTextColor();
+
+var repository = pokemonRepository.getAll();
 
 repository.forEach(function(pokemonDetail) {
   document.write('<h1>' + pokemonDetail.name + '</h1>');
-  document.write('<p>' + pokemonDetail.height) + '</p>';
-  if (pokemonDetail.height > 11) {
-    document.write(' - Wow, that\’s big!');
+  document.write('height: ' + pokemonDetail.height);
+  if (pokemonDetail.height > 0.6) {
+    document.write(' - "Wow, that\’s big!"');
   }
+  document.write('<br>')
+  document.write('type:');
   var type = pokemonDetail.types;
   type.forEach(function(pokemonType) {
-    document.write('<div ' + 'class="' + pokemonType + '" > ' + pokemonType + ' </div>');
+    document.write(pokemonRepository.getTextColor(pokemonType));
   });
 });
